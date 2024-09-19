ed32289983065adce3ed5132811a54b352e973c4
Indra Tinot-Patole <indra.tinot.patole@gmail.com>
2024-09-19 11:32:46 +0200

    Correction de la fonction sum pour passer les tests

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,9 +1,9 @@
 const sum = require("../sum");
 
 test("adds 1 + 2 to equal 3", () => {
-	expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
 
-test("adds 5 + 7 to equal 10", () => {
-	expect(sum(5, 7)).toBe(10);
+test("adds 5 + 7 to equal 12", () => {
+  expect(sum(5, 7)).toBe(12);
 });
